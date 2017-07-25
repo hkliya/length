db3b60a0a52df71341659af8610a2ebebccddb1d
Seaborn Lee <seabornlee.cn@gmail.com>
2017-07-25 11:42:22 +0800

    Refactor

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -10,25 +10,10 @@ public class Length {
     @Override
     public boolean equals(Object obj) {
         Length anotherLength = (Length) obj;
-        if (this.unit.equals(anotherLength.unit)) {
-            return this.value == anotherLength.value;
-        }
         return toMinimumUnitValue() == anotherLength.toMinimumUnitValue();
     }
 
     private int toMinimumUnitValue() {
-        if (this.unit.equals(Unit.MILE)) {
-            return this.value * 1760 * 3 * 12;
-        }
-
-        if (this.unit.equals(Unit.YARD)) {
-            return this.value * 3 * 12;
-        }
-
-        if (this.unit.equals(Unit.FEET)) {
-            return this.value * 12;
-        }
-
-        return this.value;
+        return this.value * unit.conversionRate();
     }
 }
--- src/main/java/Unit.java
+++ src/main/java/Unit.java
@@ -1,3 +1,16 @@
 public enum  Unit {
-    YARD, FEET, INCH, MILE
+    INCH(1),
+    FEET(12 * INCH.conversionRate),
+    YARD(3 * FEET.conversionRate),
+    MILE(1760 * YARD.conversionRate);
+
+    private int conversionRate;
+
+    Unit(int conversionRate) {
+        this.conversionRate = conversionRate;
+    }
+
+    public int conversionRate() {
+        return conversionRate;
+    }
 }
